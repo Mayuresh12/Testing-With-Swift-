bcfe6de108668650d6a53490ae2d89f2303bacd5
MRAO0212 <mayureshvrao@gmail.com>
2020-08-22 01:02:11 -0700

    make minor correction for fixing the the test run

--- TaylorSwiftSOng/TaylorSwiftSOng/Haters.swift
+++ TaylorSwiftSOng/TaylorSwiftSOng/Haters.swift
@@ -1,22 +1,20 @@
 //
-//  Hater.swift
-//  TaylorSwiftSOng
+//  Haters.swift
+//  TaylorSwiftSOngTests
 //
-//  Created by Mayuresh Rao on 8/5/20.
+//  Created by Mayuresh Rao on 8/22/20.
 //  Copyright © 2020 Mayuresh Rao. All rights reserved.
 //
 
 import Foundation
-
 struct Hater {
     var hating = false
-
+    
     mutating func hadABadDay() {
         hating = true
     }
-
+    
     mutating func hadAGoodDay() {
         hating = false
     }
 }
-
